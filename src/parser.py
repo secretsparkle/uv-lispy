4ac8492cfb3c231a08ff2b584f26375a76517248
Scott Madera <scott.madera@gmail.com>
2022-02-08 14:46:58 -0800

    Renovating parser

--- src/parser.py
+++ src/parser.py
@@ -1,18 +1,86 @@
-
-class RoseNode:
-    def __init__(self, value):
-        self.children = []
-        self.parent = None
-        self.value = value
-
-    def add_child(self, child):
-        self.children.append(child)
-
-class RoseTree:
-    def __init__(self, value):
-       self.root = RoseNode(value)
+import re
+from scanner import Token
+from scanner import TokenType
 
 class Parser:
-    pass
+    def __init__(self, tokens):
+        self.current = 0
+        self.tokens = tokens
 
+    def is_BODY(self):
+        while not is_EOF():
+            self.is_S_EXPRESSION()
 
+    def is_EOF(self):
+        if self.tokens[self.current].token_type == EOF:
+            return True
+        else:
+            return False
+
+    def is_IDENTIFIER(self):
+        if self.tokens[self.current].token_type == IDENTIFIER:
+            self.next_token()
+            return True
+        else:
+            return False
+
+    def is_LEFT_PAREN(self):
+        if self.tokens[self.current].token_type == LEFT_PAREN:
+            self.next_token()
+            return True
+        else:
+            return False
+
+    def is_LIST(self):
+        if not self.is_open_paren():
+            return False
+        if not self.is_identifier():
+            return False
+
+        while True:
+            self.get_next()
+            if self.is_list() or self.is_number() or self.is_identifier():
+                continue
+            else:
+                break
+        if self.is_closed_paren() and self.no_more_input():
+            return True
+        elif not self.is_closed_paren():
+            return False
+
+    def is_NUMBER(self):
+        if self.tokens[self.current].token_type == NUMBER:
+            self.next_token()
+            return True
+        else:
+            return False
+
+    def is_RIGHT_PAREN(self):
+        if self.tokens[self.current].token_type == RIGHT_PAREN:
+            self.next_token()
+            return True
+        else:
+            return False
+
+    def is_S_EXPRESSION(self):
+        pass
+
+    def is_STRING(self):
+        if self.tokens[self.current].token_type == STRING:
+            self.next_token()
+            return True
+        else:
+            return False
+
+    def next_token(self):
+        self.current += 1
+
+    def is_valid_lisp_syntax(self):
+        if self.no_more_input():
+            return False
+        if not self.balanced_parentheses():
+            return False
+        if self.is_list() or self.is_identifier() or self.is_number() or self.is_string():
+            return True
+        else:
+            return False
