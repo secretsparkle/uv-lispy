46a00f3791fb16e0b052694eaa5c560409f275e0
Scott Madera <scott.madera@gmail.com>
2022-02-27 20:09:06 -0800

    buffing out the standard library

--- library/standard.py
+++ library/standard.py
@@ -1,12 +1,18 @@
 
 def first(lst):
-    if len(lst) == 0:
-        return None
-    else:
+    if type(lst) is list and len(lst) > 0:
         return lst[0]
+    else:
+        return None
 
 def rest(lst):
-    if len(lst) <  2:
-        return None
-    else:
+    if type(lst) is list and len(lst) < 2:
         return lst[1:]
+    else:
+        return None
+
+def cons(elt, lst):
+    if type(lst) is list:
+        return lst.insert(0, elt)
+    else:
+        return None
